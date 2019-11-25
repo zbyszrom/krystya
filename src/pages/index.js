cbe8da3f74ad02f202f13346ff51557ac7bf431a
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2019-11-25 12:04:19 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Cześć Krystya</h1>
+    <p>Witaj na swojej stronie testowej</p>
+    <p>Będziemy ją stopniowo budować</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Druga strona testowa</Link>
+  </Layout>
+)
+
+export default IndexPage
